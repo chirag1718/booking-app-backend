8c58935f3dffc60e1124703052aeaeed61a8158d
Chirag <chiragsonar1718@gmail.com>
2023-05-27 18:07:46 +0530

    Feat: added update, delete and get routes for hotels

--- routes/hotels.js
+++ routes/hotels.js
@@ -2,6 +2,7 @@ import express from "express";
 import Hotel from "../model/Hotel.js";
 const router = express.Router();
 
+// Create
 router.post("/", async (req, res) => {
   const newHotel = new Hotel(req.body);
   try {
@@ -12,4 +13,47 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Update
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel Deleted Successfully!");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// Get
+router.get("/:id", async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// Get All
+router.get("/", async (req, res) => {
+  try {
+    const hotel = await Hotel.find();
+    res.status(200).json(hotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 export default router;
