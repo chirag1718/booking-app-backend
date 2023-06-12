607666ae77d447aa9aa3c1225a82b04f37a9ec71
Chirag <chiragsonar1718@gmail.com>
2023-06-12 07:40:20 +0530

    feat: destructured min, max and others from req.query object to find hotels with cheapestPrice in context to min and max queries.

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -49,8 +49,12 @@ export const getHotel = async (req, res, next) => {
 
 // Get All
 export const getHotels = async (req, res, next) => {
+  const { min, max, ...others } = req.query;
   try {
-    const hotels = await Hotel.find(req.query).limit(req.query.limit);
+    const hotels = await Hotel.find({
+      ...others,
+      cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+    }).limit(req.query.limit);
     res.status(200).json(hotels);
   } catch (err) {
     next(err);
